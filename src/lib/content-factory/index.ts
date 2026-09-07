b63724c273a82244ab43c9d5e6dd4afc220c1882
tisan82 <tisan8212@gmail.com>
2026-09-07 18:54:01 +0900

    feat: expose content factory runtime policies

---
+++ src/lib/content-factory/index.ts
@@ -0,0 +1,2 @@
+export { classifyFactoryFailure, shouldContinueFactory, CANDIDATE_FAILURE_CODES, GLOBAL_FATAL_CODES } from "./failure-policy";
+export { assertUniqueTopic, isDuplicateTopic } from "./duplicate-guard";
