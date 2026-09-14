ae005b896161c12c076f6919c162ba192cdc8065
tisan82 <tisan8212@gmail.com>
2026-09-15 00:19:30 +0900

    feat: add related content discovery

--- src/features/content/index.ts
+++ src/features/content/index.ts
@@ -1,2 +1,3 @@
 export { ContentBlockRenderer } from "@/features/content/components/ContentBlockRenderer";
 export { ContentList } from "@/features/content/components/ContentList";
+export { RelatedContentGuides } from "@/features/content/components/RelatedContentGuides";
